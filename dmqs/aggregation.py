22d6f000dfc7b51033e69bf5d5622d5255f393f7
Felipe cruz <felipecruz@loogica.net>
2012-07-03 22:59:54 -0300

    Max, Min, Sum and Count basic implementation

--- dmqs/aggregation.py
+++ dmqs/aggregation.py
@@ -1,3 +1,4 @@
+from dmqs.foundation import get_attribute
 from operator import attrgetter
 
 class Avg(object):
@@ -5,4 +6,60 @@ class Avg(object):
         self.property_name = property_name
 
     def return_value(self, data):
-        return float(sum([d.__dict__[self.property_name] for d in data])/len(data))
+        return float(sum([get_attribute(d, self.property_name)
+                          for d in data])/len(data))
+
+class Max(object):
+    def __init__(self, property_name):
+        self.property_name = property_name
+
+    def return_value(self, data):
+        return max([get_attribute(d, self.property_name) for d in data])
+
+class Min(object):
+    def __init__(self, property_name):
+        self.property_name = property_name
+
+    def return_value(self, data):
+        return min([get_attribute(d, self.property_name) for d in data])
+
+class Sum(object):
+    def __init__(self, property_name):
+        self.property_name = property_name
+
+    def return_value(self, data):
+        return sum([get_attribute(d, self.property_name) for d in data])
+
+class Std(object):
+    def __init__(self, property_name):
+        self.property_name = property_name
+
+    def return_value(self, data):
+        raise NotImplementedError()
+'''
+        from math import sqrt
+        mean = float(sum([get_attribute(d, self.property_name)
+                          for d in data])/len(data))
+        std = 0
+        std = reduce(lambda x, y: x + (y-mean)**2,
+                     [get_attribute(d, self.property_name) for d in data])
+        std = sqrt(std / float(len(data)-1))
+        return std
+'''
+
+class Var(object):
+    def __init__(self, property_name):
+        self.property_name = property_name
+
+    def return_value(self, data):
+        raise NotImplementedError()
+
+class Count(object):
+    def __init__(self, property_name):
+        self.property_name = property_name
+
+    def return_value(self, data):
+        for d in data:
+            d.__dict__["{0}__count".format(self.property_name)] = \
+                                    len(get_attribute(d, self.property_name))
+        return data
--- dmqs/tests/test_aggregation.py
+++ dmqs/tests/test_aggregation.py
@@ -1,4 +1,6 @@
-from dmqs.aggregation import Avg
+import pytest
+
+from dmqs.aggregation import Avg, Max, Min, Sum, Var, Std, Count
 
 def type_instance(type_name, prop, value):
     instance_type = type(type_name, (object,), {})
@@ -22,19 +24,117 @@ def test_aggregation_agv():
 
 
 def test_aggregation_min():
-    pass
+    model1 = type_instance('Student', 'grade', 9.0)
+    model2 = type_instance('Student', 'grade', 8.0)
+    model3 = type_instance('Student', 'grade', 2.0)
+    model4 = type_instance('Student', 'grade', 3.0)
+    model5 = type_instance('Student', 'grade', 7.0)
+    model6 = type_instance('Student', 'grade', 1.0)
+
+    models = [model1, model2, model3, model4, model5, model6]
+
+    min = Min('grade')
+
+    assert min.return_value(models) == 1.0
 
 def test_aggregation_max():
-    pass
+    model1 = type_instance('Student', 'grade', 9.0)
+    model2 = type_instance('Student', 'grade', 8.0)
+    model3 = type_instance('Student', 'grade', 2.0)
+    model4 = type_instance('Student', 'grade', 3.0)
+    model5 = type_instance('Student', 'grade', 7.0)
+    model6 = type_instance('Student', 'grade', 1.0)
+
+    models = [model1, model2, model3, model4, model5, model6]
+
+    max = Max('grade')
+
+    assert max.return_value(models) == 9.0
 
 def test_aggregation_count():
-    pass
+    def type_and_instance(type_name, **kwargs):
+        new_class = type(type_name, (object,), {})
+        instance = new_class()
+        instance.__dict__ = kwargs
+        return instance
+
+    account1 = type_and_instance('Account',
+                                credit=300)
+    account2 = type_and_instance('Account',
+                                credit=200)
+    account3 = type_and_instance('Account',
+                                credit=100)
+
+    accounts = [account1, account2, account3]
+
+    account11 = type_and_instance('Account',
+                                credit=300)
+    account12 = type_and_instance('Account',
+                                credit=200)
+
+    accounts1 = [account11, account12]
+
+    person = type_and_instance('Person',
+                               name="Name 4",
+                               nickname="Nickname 4",
+                               accounts=accounts,
+                               age=1,
+                               memory=True)
+
+    person1 = type_and_instance('Person',
+                               name="Name 4",
+                               nickname="Nickname 4",
+                               accounts=accounts1,
+                               age=1,
+                               memory=True)
+
+    count = Count('accounts')
+
+    data = [person, person1]
+
+    assert count.return_value(data)[0].__dict__['accounts__count']== 3
+    assert count.return_value(data)[1].__dict__['accounts__count'] == 2
 
 def test_aggregation_stdev():
-    pass
+    model1 = type_instance('Student', 'grade', 9.0)
+    model2 = type_instance('Student', 'grade', 8.0)
+    model3 = type_instance('Student', 'grade', 2.0)
+    model4 = type_instance('Student', 'grade', 3.0)
+    model5 = type_instance('Student', 'grade', 7.0)
+    model6 = type_instance('Student', 'grade', 1.0)
+
+    models = [model1, model2, model3, model4, model5, model6]
+
+    std = Std('grade')
+
+    with pytest.raises(NotImplementedError):
+        assert std.return_value(models) == 3.1
 
 def test_aggregation_variance():
-    pass
+    model1 = type_instance('Student', 'grade', 9.0)
+    model2 = type_instance('Student', 'grade', 8.0)
+    model3 = type_instance('Student', 'grade', 2.0)
+    model4 = type_instance('Student', 'grade', 3.0)
+    model5 = type_instance('Student', 'grade', 7.0)
+    model6 = type_instance('Student', 'grade', 1.0)
+
+    models = [model1, model2, model3, model4, model5, model6]
+
+    var = Var('grade')
+
+    with pytest.raises(NotImplementedError):
+        assert var.return_value(models) == 3.1
 
 def test_aggregation_sum():
-    pass
+    model1 = type_instance('Student', 'grade', 9.0)
+    model2 = type_instance('Student', 'grade', 8.0)
+    model3 = type_instance('Student', 'grade', 2.0)
+    model4 = type_instance('Student', 'grade', 3.0)
+    model5 = type_instance('Student', 'grade', 7.0)
+    model6 = type_instance('Student', 'grade', 1.0)
+
+    models = [model1, model2, model3, model4, model5, model6]
+
+    sum = Sum('grade')
+
+    assert sum.return_value(models) == 30.0
