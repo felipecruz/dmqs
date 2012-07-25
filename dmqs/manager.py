8fe511f67fad9bb9168c833e4215482bbb76a87e
Felipe cruz <felipecruz@loogica.net>
2012-07-25 12:24:22 -0300

    Allow manager to be created with default filters
    add "add()" and __len__() methods to manager

--- dmqs/manager.py
+++ dmqs/manager.py
@@ -6,19 +6,32 @@ from repository import Repository
 repository = Repository()
 
 class MemoryManager(Manager):
-    def __init__(self, model):
+    def __init__(self, model, default_filters=None):
         self.model_name = model.__name__
         self.model = model
+        self.default_filters = default_filters
 
     def get_query_set(self):
         """Returns a new QuerySet object.  Subclasses can override this method
         to easily customize the behavior of the Manager.
         """
         if self.model_name in repository.get_names():
-            return MemoryQuerySet(self.model,
+            if self.default_filters:
+                return MemoryQuerySet(self.model,
+                                      data=repository.\
+                                              get_models(self.model_name)
+                                     ).filter(**self.default_filters)
+            else:
+                return MemoryQuerySet(self.model,
                                   data=repository.get_models(self.model_name))
 
         return MemoryQuerySet(self.model)
 
     def create(self, *args, **kwargs):
         return self.model(**kwargs)
+
+    def add(self, instance):
+        repository.save(self.model_name, instance)
+
+    def __len__(self):
+        return len(self.get_query_set().data)
