c9242bbf40322b1b37c93c49f0978241c3a62127
Felipe cruz <felipecruz@loogica.net>
2012-08-24 21:59:01 -0300

    remove unnecessary try/except block

--- dmqs/integration/utils.py
+++ dmqs/integration/utils.py
@@ -2,9 +2,6 @@ def fetch_primary_key(model_instance):
     try:
         return getattr(model_instance, 'id')
     except:
-        try:
-            for field in model_instance._meta.fields:
-                if field.primary_key:
-                    return getattr(model_instance, field.name)
-        except:
-            return None
+        for field in model_instance._meta.fields:
+            if field.primary_key:
+                return getattr(model_instance, field.name)
